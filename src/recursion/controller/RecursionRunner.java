bc5d6dcfd14566e6978de255370a19e0e6ee61ce
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-03-06 12:14:56 -0700

    Added Basic Runner

---
+++ src/recursion/controller/RecursionRunner.java
@@ -0,0 +1,8 @@
+package recursion.controller;
+
+public class RecursionRunner {
+	public static void main(String [] args) {
+		RecursionController app = new RecursionController();
+		app.start();
+	}
+}
